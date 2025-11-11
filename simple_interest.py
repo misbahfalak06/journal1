b372eaae2cfe9bae14db8ac029018e8a723c70ed
misbahfalak06 <misbahfalak2306@gmail.com>
2025-11-11 11:45:22 +0530

    Update simple_interest.py

--- simple_interest.py
+++ simple_interest.py
@@ -1,9 +1,9 @@
 import sys
-if len(sys.argv) ==4:
+if len(sys.argv) == 4:
     script_name=sys.argv[0]
     principle=sys.argv[1]
     rate=sys.argv[2]
-    time=sys.argv[3]
+    duration=sys.argv[3]
     print("user provided inputs")
 else:
     script_name=sys.argv[0]
@@ -11,9 +11,9 @@ else:
     rate="5"
     time="3"
     print("default inputs")
-si=(int(principle)*float(rate)*int(time))/100
-print("Script Name:",script_name)   
-print("Principle Amount:",principle)
-print("Rate of Interest:",rate)
-print("Time (years):",time)
-print("Simple Interest:",si)
+    si=(int(principle)*float(rate)*int(time))/100
+    print("Script Name:",script_name)   
+    print("Principle Amount:",principle)
+    print("Rate of Interest:",rate)
+    print("Time (years):",time)
+    print("Simple Interest:",si)
